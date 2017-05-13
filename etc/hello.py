e425e9e95ecfb43ba325c23a864c3163479094c4
Maslenok <masluk79@gmail.com>
2017-05-13 03:20:14 +0300

    gunicorn4

--- etc/hello.py
+++ etc/hello.py
@@ -1,1 +1,19 @@
-/etc/gunicorn.d/hello.py
+CONFIG = {
+
+  'mode': 'wsgi',
+  'working_dir': '/home/box/web',
+  'python': '/usr/bin/python3',
+  'args': (
+    '--bind=0.0.0.0:8080',
+    '--workers=2',
+    '--timeout=15',
+    '--log-level=debug',
+    'hello:app'
+  )
+}
+
+
+
+
+
+
