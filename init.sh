9156e5e919afcf0985d46fa958cb7b83c7adaecb
Maslenok <masluk79@gmail.com>
2017-05-13 03:10:44 +0300

    gunicorn3

--- init.sh
+++ init.sh
@@ -1,6 +1,23 @@
-sudo rm -v /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
-sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
+sudo /etc/init.d/gunicorn restart﻿﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
