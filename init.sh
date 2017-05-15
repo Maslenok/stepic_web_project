4daf4e3e7fe4c511b7ff33320413108d10ffac93
Maslenok <masluk79@gmail.com>
2017-05-15 22:58:45 +0300

    OrangePiGunicorn

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
-sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx -s reload
 
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo gunicorn -w 6 hello:application
-
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 
 
 
