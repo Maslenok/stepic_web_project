433ed2167046c216ef0a27832a49f460f0e0ad07
Maslenok <masluk79@gmail.com>
2017-05-16 16:38:05 +0300

    NginxConf

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx -s reload
 
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo gunicorn  -w 4  hello:application
 
 
 
